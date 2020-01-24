bf99113f9179838f883f27d4993ffee54d930b64
Matthias <deploy@travis-ci.org>
2020-01-24 21:06:07 +0000

    Deploy not-matthias/not-matthias.github.io to github.com/not-matthias/not-matthias.github.io.git:master

---
+++ precache-manifest.321aa93f1aa8d53c72b32dba7c6ead01.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6a56528cb4ff5b764ae",
+    "url": "/css/app.85b3178a.css"
+  },
+  {
+    "revision": "1b9782029a053d446da2",
+    "url": "/css/chunk-238435f2.0670aa22.css"
+  },
+  {
+    "revision": "0085b2ffde85f3952162",
+    "url": "/css/chunk-2f0ce9c1.3b281235.css"
+  },
+  {
+    "revision": "c4d914c15ba106b35b72",
+    "url": "/css/chunk-56b1e67c.a4cd4971.css"
+  },
+  {
+    "revision": "f6d4e2733427e40184f4",
+    "url": "/css/chunk-7f4e4c66.d3b413e4.css"
+  },
+  {
+    "revision": "2e224a5de2b6153e545c",
+    "url": "/css/chunk-b883bff6.7efb586f.css"
+  },
+  {
+    "revision": "b98782086e4b30f66c4c",
+    "url": "/css/chunk-c25fd278.e55fc2e6.css"
+  },
+  {
+    "revision": "99a10bc7959cc55b6952",
+    "url": "/css/chunk-c7fa703e.63747ed1.css"
+  },
+  {
+    "revision": "d086b7f6fe287acc2251",
+    "url": "/css/chunk-vendors.adce9de4.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "99a202738da3583bf08cc3f09abf6f04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6a56528cb4ff5b764ae",
+    "url": "/js/app.af1d07dd.js"
+  },
+  {
+    "revision": "1b9782029a053d446da2",
+    "url": "/js/chunk-238435f2.df123b81.js"
+  },
+  {
+    "revision": "081216257cac1d844b87",
+    "url": "/js/chunk-2d0ac1cc.2f816026.js"
+  },
+  {
+    "revision": "60a7b37f5c1543ea8a43",
+    "url": "/js/chunk-2d0bd977.69357e71.js"
+  },
+  {
+    "revision": "c51424d0ca2c2dc81afa",
+    "url": "/js/chunk-2d0c1d78.c040bdfa.js"
+  },
+  {
+    "revision": "c15f4b806fb24b6f5833",
+    "url": "/js/chunk-2d0e9cd9.847be602.js"
+  },
+  {
+    "revision": "cf0ceaae574167bf08f0",
+    "url": "/js/chunk-2d208bfd.1a5a6906.js"
+  },
+  {
+    "revision": "0085b2ffde85f3952162",
+    "url": "/js/chunk-2f0ce9c1.dbcf91e5.js"
+  },
+  {
+    "revision": "c4d914c15ba106b35b72",
+    "url": "/js/chunk-56b1e67c.0ca060e5.js"
+  },
+  {
+    "revision": "f6d4e2733427e40184f4",
+    "url": "/js/chunk-7f4e4c66.66bedd05.js"
+  },
+  {
+    "revision": "2e224a5de2b6153e545c",
+    "url": "/js/chunk-b883bff6.08d7358f.js"
+  },
+  {
+    "revision": "b98782086e4b30f66c4c",
+    "url": "/js/chunk-c25fd278.5f715000.js"
+  },
+  {
+    "revision": "99a10bc7959cc55b6952",
+    "url": "/js/chunk-c7fa703e.cbf494ef.js"
+  },
+  {
+    "revision": "d086b7f6fe287acc2251",
+    "url": "/js/chunk-vendors.acce74c9.js"
+  },
+  {
+    "revision": "2423218fb10ec29bb292db783d3490ee",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.23382cfacf641bc58d045661e8dff3af.js"
+  "/precache-manifest.321aa93f1aa8d53c72b32dba7c6ead01.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-blog"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "vue-blog"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
